31796dea0c969d019d87af22a593d798312427b8
Spike2250 <shilov.spike2250@yandex.ru>
2023-07-24 17:17:57 +0500

    refactoring

--- gendiff/diff/diff.py
+++ gendiff/diff/diff.py
@@ -7,10 +7,10 @@ from gendiff.diff.analyse import analyse_diff_files
 def generate_diff(args) -> str:
     try:
         if args.format == "stylish":
-            format_1 = parse_format(args.first_file)
-            format_2 = parse_format(args.second_file)
-            file1 = read_file(args.first_file, format_1)
-            file2 = read_file(args.second_file, format_2)
+            file1 = read_file(args.first_file,
+                              parse_format(args.first_file))
+            file2 = read_file(args.second_file,
+                              parse_format(args.second_file))
         else:
             file1, file2 = read_files(args.first_file,
                                       args.second_file,
