518bb00ebcdb6ea5c3fe0656c88d62fa49afd80c
Spike2250 <shilov.spike2250@yandex.ru>
2023-08-02 13:33:30 +0500

    refactoring diff.py

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -31,6 +31,49 @@ def analyse_diff_files(dict1, dict2) -> list:  # noqa: C901
             return Diff(ADDED, key,
                         iter_(dict2.get(key), dict2.get(key)))
 
+        def iter_key_in_both_dict(key, values_are_same,
+                                  both_values_is_dict,
+                                  both_values_isnt_dict):
+            if values_are_same:
+                iter_values_are_same(key, both_values_is_dict,
+                                     both_values_isnt_dict)
+            else:
+                iter_values_arent_same(key, both_values_is_dict,
+                                       both_values_isnt_dict)
+
+        def iter_values_are_same(key, both_values_is_dict,
+                                 both_values_isnt_dict):
+            if both_values_isnt_dict:
+                diff.append(not_changed_value(key))
+            elif both_values_is_dict:
+                diff.append(same_dicts_iter(key))
+
+        def iter_values_arent_same(key, both_values_is_dict,
+                                   both_values_isnt_dict):
+            if both_values_isnt_dict:
+                diff.append(remove_value(key))
+                diff.append(add_value(key))
+            elif both_values_is_dict:
+                diff.append(same_dicts_iter(key))
+            elif isinstance(dict1.get(key), dict):
+                diff.append(remove_dict_iter(key))
+                diff.append(add_value(key))
+            else:
+                diff.append(remove_value(key))
+                diff.append(add_dict_iter(key))
+
+        def iter_key_in_dict1(key):
+            if isinstance(dict1.get(key), dict):
+                diff.append(remove_dict_iter(key))
+            else:
+                diff.append(remove_value(key))
+
+        def iter_key_in_dict2(key):
+            if isinstance(dict2.get(key), dict):
+                diff.append(add_dict_iter(key))
+            else:
+                diff.append(add_value(key))
+
         diff = []
         keys = list(set(list(dict1.keys()) + list(dict2.keys())))
 
@@ -43,33 +86,13 @@ def analyse_diff_files(dict1, dict2) -> list:  # noqa: C901
                                          not isinstance(dict2.get(key), dict)))
 
             if key_in_both_dict:
-                if values_are_same:
-                    if both_values_isnt_dict:
-                        diff.append(not_changed_value(key))
-                    elif both_values_is_dict:
-                        diff.append(same_dicts_iter(key))
-                else:
-                    if both_values_isnt_dict:
-                        diff.append(remove_value(key))
-                        diff.append(add_value(key))
-                    elif both_values_is_dict:
-                        diff.append(same_dicts_iter(key))
-                    elif isinstance(dict1.get(key), dict):
-                        diff.append(remove_dict_iter(key))
-                        diff.append(add_value(key))
-                    else:
-                        diff.append(remove_value(key))
-                        diff.append(add_dict_iter(key))
+                iter_key_in_both_dict(key, values_are_same,
+                                      both_values_is_dict,
+                                      both_values_isnt_dict)
             elif key in dict1:
-                if isinstance(dict1.get(key), dict):
-                    diff.append(remove_dict_iter(key))
-                else:
-                    diff.append(remove_value(key))
+                iter_key_in_dict1(key)
             elif key in dict2:
-                if isinstance(dict2.get(key), dict):
-                    diff.append(add_dict_iter(key))
-                else:
-                    diff.append(add_value(key))
+                iter_key_in_dict2(key)
         return diff
 
     return iter_(dict1, dict2)
